e55d3244f20f50fddde577fbe88fb1030c649658
lewyonq <jakub.lewandowski98@gmail.com>
2023-06-14 09:44:30 +0200

    server initialization

---
+++ apps/server/src/main/java/com/lifetrinkets/lifetrinkets/LifetrinketsApplication.java
@@ -0,0 +1,13 @@
+package com.lifetrinkets.lifetrinkets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifetrinketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifetrinketsApplication.class, args);
+    }
+
+}
---
+++ apps/server/src/test/java/com/lifetrinkets/lifetrinkets/LifetrinketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifetrinkets.lifetrinkets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifetrinketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
